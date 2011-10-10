c487eecdd8986dfade474f7e89754e5df53a841e
Jeff Sheffield <jeff.sheffield@gmail.com>
2011-10-09 19:53:20 -0500

    added twisted docs

---
+++ twisted/reactor/calllater.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+from twisted.internet import reactor
+import time
+
+
+def printTime():
+    print "The current time is", time.strftime("%H:%M:%S")
+
+def stopReactor():
+    print "Stopping reactor"
+    reactor.stop()
+
+reactor.callLater(1, printTime)
+reactor.callLater(2, printTime)
+reactor.callLater(3, printTime)
+reactor.callLater(4, printTime)
+reactor.callLater(5, printTime)
+reactor.callLater(6, printTime)
+
+print "Running the reactor"
+reactor.run()
+print "Reactor stopped."
+
+
